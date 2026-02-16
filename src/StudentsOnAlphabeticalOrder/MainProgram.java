4ab11c73a86a94cc2b17229f89c54d189f45fc77
gui <gui151208@gmail.com>
2026-02-16 16:42:46 -0300

    mooc exercise

---
+++ src/StudentsOnAlphabeticalOrder/MainProgram.java
@@ -0,0 +1,10 @@
+package StudentsOnAlphabeticalOrder;
+
+public class MainProgram {
+
+    public static void main(String[] args) {
+//        Student first = new Student("jamo");
+//        Student second = new Student("jamo1");
+//        System.out.println(first.compareTo(second));
+    }
+}
