5eb49fafca49833a53db9651afbbfb0069a6b571
gui <gui151208@gmail.com>
2026-02-17 16:48:23 -0300

    mooc exercise

---
+++ src/SortCards/BySuitInValueOrder.java
@@ -0,0 +1,14 @@
+package SortCards;
+
+import java.util.Comparator;
+
+public class BySuitInValueOrder implements Comparator<Card> {
+
+    @Override
+    public int compare(Card c1, Card c2) {
+        if (c1.getSuit() != c2.getSuit()) {
+            return c1.getValue() - c2.getValue();
+        }
+        return 0;
+    }
+}
--- src/SortCards/Hand.java
+++ src/SortCards/Hand.java
@@ -4,15 +4,18 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Hand {
+public class Hand implements Comparable<Hand> {
     List<Card> cards;
+    int handRank;
 
     public Hand() {
         this.cards = new ArrayList<>();
+        this.handRank = 0;
     }
 
     public void add(Card card) {
         cards.add(card);
+        calcRank();
     }
 
     public void print() {
@@ -21,5 +24,24 @@ public class Hand {
 
     public void sort() {
         Collections.sort(cards);
+    }
+
+    public void sortBySuitInValueOrder() {
+        Collections.sort(cards, new BySuitInValueOrder());
+    }
+
+    public int getHandRank() {
+        return handRank;
+    }
+
+    private void calcRank () {
+        for (Card card : cards) {
+            handRank += card.getValue();
+        }
+    }
+
+    @Override
+    public int compareTo(Hand hand) {
+        return hand.getHandRank() - this.getHandRank();
     }
 }
