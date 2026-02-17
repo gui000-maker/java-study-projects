a57e8316e0033bc70efe2617e00d81e8ad99afce
gui <gui151208@gmail.com>
2026-02-17 16:21:34 -0300

    mooc exercise

--- src/EnumIterator/Main.java
+++ src/EnumIterator/Main.java
@@ -2,7 +2,17 @@ package EnumIterator;
 
 public class Main {
     static void main() {
-        Person anna = new Person("Anna", Education.PHD);
-        System.out.println(anna);
+        Employees university = new Employees();
+        university.add(new Person("Petrus", Education.PHD));
+        university.add(new Person("Arto", Education.HS));
+        university.add(new Person("Elina", Education.PHD));
+
+        university.print();
+
+        university.fire(Education.HS);
+
+        System.out.println("==");
+
+        university.print();
     }
 }
