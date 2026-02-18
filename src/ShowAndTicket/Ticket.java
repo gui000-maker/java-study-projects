d08b3e3dd0c07fcae5231d7e4c6540bf32827bb8
gui <gui151208@gmail.com>
2026-02-18 17:22:44 -0300

    mooc exercise

--- src/ShowAndTicket/Ticket.java
+++ src/ShowAndTicket/Ticket.java
@@ -3,4 +3,10 @@ package ShowAndTicket;
 public class Ticket {
     private int seat;
     private int code;
+    private int quantity;
+    private Show show;
+
+    public Ticket(Show show) {
+        this.show = show;
+    }
 }
