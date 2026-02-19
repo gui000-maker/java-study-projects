0eb88f0719dcced4c9a082652328629353f09078
gui <gui151208@gmail.com>
2026-02-19 20:21:39 -0300

    mooc exercise

--- src/ValidatingParameters/Calculator.java
+++ src/ValidatingParameters/Calculator.java
@@ -3,6 +3,9 @@ package ValidatingParameters;
 public class Calculator {
 
     public int factorial(int num) {
+        if (num < 0) {
+            throw new IllegalArgumentException("Negative number");
+        }
 
         int answer = 1;
         for (int i = 1; i <= num; i++) {
@@ -13,6 +16,13 @@ public class Calculator {
     }
 
     public int binomialCoefficent(int setSize, int subsetSize) {
+        if (setSize < 0 || subsetSize < 0) {
+            throw new IllegalArgumentException("Negative number");
+        }
+
+        if (subsetSize > setSize) {
+            throw new IllegalArgumentException("Negative number");
+        }
 
         int numerator = factorial(setSize);
         int denominator = factorial(subsetSize) * factorial(setSize - subsetSize);
