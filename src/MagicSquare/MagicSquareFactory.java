d416aa46c5e3e72ddfb44ef6924d256ce48b3a29
gui <gui151208@gmail.com>
2026-02-22 16:44:50 -0300

    mooc exercise

---
+++ src/MagicSquare/MagicSquareFactory.java
@@ -0,0 +1,13 @@
+package MagicSquare;
+
+public class MagicSquareFactory {
+
+    public MagicSquare createMagicSquare(int size) {
+
+        MagicSquare square = new MagicSquare(size);
+
+        // implement the creation of a magic square with the Siamese method algorithm here
+        return square;
+    }
+
+}
